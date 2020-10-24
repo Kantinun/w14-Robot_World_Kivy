2adefb0ed62a086b7cd5d9fc6a9297a9752a9b3a
Phimbun Issawilanon <s6201012620198@email.kmutnb.ac.th>
2020-10-24 22:35:45 +0700

    Draw Robot

--- robotWorld.py
+++ robotWorld.py
@@ -1,14 +1,25 @@
+from typing import Sized
 from kivy.app import App
 from kivy.core import window
 from kivy.uix.widget import Widget
 from kivy.graphics import Rectangle
 from kivy.core.window import Window
 
+class Robot(Widget):
+      def  __init__(self, ro, co, size):
+        self.size = (size,size)
+        self.x = Window.width/(co*2)-self.size[0]/2
+        self.y = Window.height/(ro*2)-self.size[1]/2
+
+      def draw(self):
+            Rectangle(pos=(self.x,self.y),size=self.size)  
+          
 class World(Widget):
     def __init__(self,r, c,**kwargs):
         super().__init__(**kwargs)
         self.row = int(r)
         self.column = int(c)
+        ro = Robot(self.row, self.column, 100)
 
         with self.canvas:
           for i in range(1,self.column+1):
@@ -16,6 +27,8 @@ class World(Widget):
             
           for i in range(1,self.row+1):
             Rectangle(pos=(0,i*Window.height/self.row),size=(Window.width,2))
+          
+          ro.draw()          
 
 class RobotWorld(App):
     def build(self):
